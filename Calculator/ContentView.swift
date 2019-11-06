e1a9dc7b3c3abbfb22058dae8a07e55958273763
Ei Phyu <eiphyu672000@gmail.com>
2019-11-06 16:38:51 +0630

    Initial Commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Ei Phyu on 06/11/2019.
+//  Copyright © 2019 Ei Phyu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
